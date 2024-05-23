b7a7f2796485ace0271ad3adf3226ddfeed8f589
Satyam Kumar Sharma <satyamkumarsharma849@gmail.com>
2024-05-23 22:40:15 +0530

    .

---
+++ 01basicsreactapp/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Chai from "./chai"
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>Hello Chai aur Code.</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+function Chai() {
+  return (
+    <h1>Hello Chai aur Code.</h1>
+  );
+}
+
+export default Chai;
